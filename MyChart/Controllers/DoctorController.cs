dfd6fcf25891a2c37b6a1b52f7055d834605947f
LJL <781867500@qq.com>
2018-07-09 18:38:45 +0800

    2018-07-09 demo

---
+++ MyChart/Controllers/DoctorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyChart.Controllers
+{
+    public class DoctorController : Controller
+    {
+        //
+        // GET: /Doctor/
+
+        public ActionResult DoctorLine()
+        {
+            return View();
+        }
+        
+        public ActionResult DoctorPie()
+        {
+            return View();
+        }
+
+    }
+}
